8c1891267e990fcf103a8f41e06746f042df0407
properteaseph-wq <properteaseph@gmail.com>
2026-02-22 21:45:43 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE = "propertease-pwa-v1";
+const CACHE = "propertease-pwa-v2";
+
 const ASSETS = [
   "./",
   "./index.html",
@@ -10,13 +11,23 @@ const ASSETS = [
   "./js/ui.js",
   "./js/prompt-engine.js"
 ];
+
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
