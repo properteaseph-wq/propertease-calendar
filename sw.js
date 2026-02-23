da9031d8a63ff0e36f9c2fa2444dd4780f3af13a
properteaseph-wq <properteaseph@gmail.com>
2026-02-22 19:15:50 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
 const CACHE = "propertease-pwa-v1";
-const ASSETS = ["./", "./index.html", "./app.css", "./app.js", "./manifest.webmanifest"];
-
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./css/app.css",
+  "./js/app.js",
+  "./js/db.js",
+  "./js/ui.js",
+  "./js/prompt-engine.js"
+];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
   self.skipWaiting();
