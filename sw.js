e6a23fa5fa9132cd3149289a27d056dc084513ac
properteaseph-wq <properteaseph@gmail.com>
2026-02-22 18:42:11 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "propertease-pwa-v1";
+const ASSETS = ["./", "./index.html", "./app.css", "./app.js", "./manifest.webmanifest"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
